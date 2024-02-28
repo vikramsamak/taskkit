e8a86198fd1c5e1674f2e24eb38dc8cdf5488377
vikramsamak <vikramsamak02@gmail.com>
2024-02-28 18:35:09 +0530

    Created initial ui for todos.

---
+++ src/components/Todos/AddTodo.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+function AddTodo() {
+  return <div className="h-full ">AddTodo</div>;
+}
+
+export default AddTodo;
---
+++ src/components/Todos/DeleteTodo.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+function DeleteTodo() {
+  return <div className="h-full ">DeleteTodo</div>;
+}
+
+export default DeleteTodo;
